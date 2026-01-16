08e00dd1b97b1f9a812d786f8f2ffad8a4f07d59
berylatieno <berylatieno30@gmail.com>
2026-01-16 15:22:14 +0300

    feat: routes setup

---
+++ server/internal/routes/customer.go
@@ -0,0 +1,9 @@
+package routes
+
+// TODO: GET /services - list services by business
+// TODO: GET /timeslots?business_id= - get available time slots for a business
+// TODO: GET /services - list services by business
+// TODO: POST /appointments - book appointment
+// TODO: POST /payments - pay for an appointment
+// TODO: GET /appointments/:id - get appointment details
+// TODO: GET /appointments/ - get all appointments
